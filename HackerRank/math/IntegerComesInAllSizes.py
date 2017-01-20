485e9553a91b6aa801ec3df2974cf0bee8041359
holi87 <holi87@gmail.com>
2017-01-21 00:06:06 +0100

    rozwiazanie poprawne

---
+++ HackerRank/math/IntegerComesInAllSizes.py
@@ -0,0 +1,6 @@
+pierwszaPodstawa = int(input())
+pierwszaPotega = int(input())
+drugaPodstawa = int(input())
+drugaPotega = int(input())
+
+print(pierwszaPodstawa ** pierwszaPotega + drugaPodstawa ** drugaPotega)
