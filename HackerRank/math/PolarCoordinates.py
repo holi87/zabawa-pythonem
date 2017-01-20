e6da9065086a06198e248ab942c6da018ff3b836
holi87 <holi87@gmail.com>
2017-01-20 23:54:16 +0100

    rozwiazanie poprawne

---
+++ HackerRank/math/PolarCoordinates.py
@@ -0,0 +1,9 @@
+from cmath import *
+# obliczenia dotyczące liczb zespolonych a dokładniej ich wspolrzednych
+
+liczba = input()
+
+print(abs(complex(liczba)))  # liczy dlugosc absolutna od punktu 0.0
+
+print(phase(complex(liczba)))  # a to liczy kąt
+# obu nie mam pojęcia jak i dlaczego :D
