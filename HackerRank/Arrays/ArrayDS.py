c6b2e965c93c9a2d358c1ec11059078b8375a623
holi87 <holi87@gmail.com>
2017-01-22 22:10:12 +0100

    zabawa tablica

---
+++ HackerRank/Arrays/ArrayDS.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import sys
+
+
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+arr = list(reversed(arr))
+for i in range(n-1):
+    print("%d " % arr[i], end="")
+print(arr[n-1], end="")
+
+
+# How to make it in hard way :D
