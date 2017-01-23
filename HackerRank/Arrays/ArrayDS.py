01ac95a3b353e82e0323fe41d33eb4a08b0e0b34
holi87 <holi87@gmail.com>
2017-01-23 10:26:10 +0100

    tak by bylo prosciej

--- HackerRank/Arrays/ArrayDS.py
+++ HackerRank/Arrays/ArrayDS.py
@@ -1,9 +1,11 @@
 n = int(input().strip())
 arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
 arr = list(reversed(arr))
-for i in range(n-1):
-    print("%d " % arr[i], end="")
-print(arr[n-1], end="")
+# for i in range(n-1):
+#     print("%d " % arr[i], end="")
+# print(arr[n-1], end="")
 
 
 # How to make it in hard way :D
+
+print(" ".join(map(str, arr)))
