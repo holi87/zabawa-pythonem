dfa788100f52038b02a0dbe67e4f865867fb0696
holi87 <holi87@gmail.com>
2017-01-24 09:47:34 +0100

    30D of Code: D7

--- HackerRank/30DaysOfCode/D7Arrays.py
+++ HackerRank/30DaysOfCode/D7Arrays.py
@@ -1,4 +1,3 @@
 n = int(input().strip())
 arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
-arr = list(reversed(arr))
-print(" ".join(map(str, arr)))
+print(" ".join(map(str, (list(reversed(arr))))))
