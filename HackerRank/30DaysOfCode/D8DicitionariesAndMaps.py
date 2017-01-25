824a52427a4e5c70bc9cf5a4575fbc7470f156a3
holi87 <holi87@gmail.com>
2017-01-25 09:44:43 +0100

    30D of Code: D8

---
+++ HackerRank/30DaysOfCode/D8DicitionariesAndMaps.py
@@ -0,0 +1,19 @@
+ilosc = int(input())
+phoneBook = {} # słownik, klucz: wartosc, kluczem bedzie imie bo po nim szukamy
+# wypelnianie slownika
+for i in range(ilosc):
+    imie, numer = input().split()
+    numer = int(numer)
+    # sprawdzenie czy poprawne wpisy
+    # print(imie)
+    # print(numer)
+    phoneBook[imie] = numer
+# sprawdzenie czy poprawnie wpisalem w slownik
+# print(phoneBook)
+# przeszukiwanie slownika
+for i in range(ilosc):
+    imieSzukane = input()
+    if imieSzukane in phoneBook:
+        print("%s=%d" % (imieSzukane, phoneBook[imieSzukane]))
+    else:
+        print("Not found")
