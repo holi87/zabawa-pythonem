c2775cabcc45414b2d41ba04207f933121e655dc
holi87 <holi87@gmail.com>
2017-01-27 20:56:26 +0100

    zmiana silni na uzycie wlasnej funkcji

--- HackerRank/30DaysOfCode/D9Recursion.py
+++ HackerRank/30DaysOfCode/D9Recursion.py
@@ -1,17 +1,22 @@
-def factorial(liczba):
-    wynik = 1
-    for i in range(1,liczba+1):
-        wynik *= i
-    print(wynik)
-
-factorial(int(input()))
-
-# przerobić na:
-# for i in range( 1, liczba+1):
-#   if liczba = 2:
-#       return 2
-#   else:
-#       return factorial(liczba-1) * liczba
+# wersja pierwsza:
+# def factorial(liczba):
+#     wynik = 1
+#     for i in range(1, liczba+1):
+#         wynik *= i
+#     print(wynik)
 #
-#
-#   print(factorial(int(input()))
+# factorial(int(input()))
+
+# wersja wg zalozen HR
+
+
+def factorial2(liczba2):
+    for i in range(1, liczba2+1):
+        if liczba2 == 2:
+            return 2
+        elif liczba2 == 1 or liczba2 == 0:
+            return 1
+        else:
+            return factorial2(liczba2-1)*liczba2
+
+print(factorial2(int(input())))
