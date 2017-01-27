ae24be4b589fe8e25b2dc4893e5e98fa6611bc8c
holi87 <holi87@gmail.com>
2017-01-27 09:25:42 +0100

    factorial inna wersja w komentarzu

---
+++ HackerRank/30DaysOfCode/D10BinaryNumbers.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import sys
+
+
+n = int(input().strip())
+binarny = str(int(bin(n)[2:]))
+# sprawdzenie czy wyrzucilem 0b
+# print(binarny)
+# split po zerach by je usunac
+
+bezZer = binarny.split("0")
+# print(bezZer)
+# sprawdzenie dlugosci elementow w calej tablicy, aby znalezc najdluzszy
+temp = 0
+for i in range(len(bezZer)):
+    if len(bezZer[i]) > temp:
+        temp = len(bezZer[i])
+
+
+print(temp)
