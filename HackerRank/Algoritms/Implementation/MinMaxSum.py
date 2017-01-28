f97b53ae4a241ad762a475fe7bbb26018b2ac54d
holi87 <holi87@gmail.com>
2017-01-28 20:50:12 +0100

    min max sum

--- HackerRank/Algoritms/Implementation/MinMaxSum.py
+++ HackerRank/Algoritms/Implementation/MinMaxSum.py
@@ -6,5 +6,5 @@ import sys
 a,b,c,d,e = input().strip().split(' ')
 a,b,c,d,e = [int(a),int(b),int(c),int(d),int(e)]
 # moj kod ponizej
-lista = [a, b, c, d, e]
-print(sum(lista)-max(lista), sum(lista)-min(lista))
+# lista = [a, b, c, d, e]
+print(sum(a, b, c, d, e)-max(a, b, c, d, e), sum(a, b, c, d, e)-min(a, b, c, d, e))
