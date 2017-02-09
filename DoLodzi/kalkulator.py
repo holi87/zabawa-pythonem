d2bb2fd71c390564b6a02378f441945d5d884a93
holi87 <holi87@gmail.com>
2017-02-09 20:34:40 +0100

    kalk do lodzi

--- DoLodzi/kalkulator.py
+++ DoLodzi/kalkulator.py
@@ -29,43 +29,33 @@ class Kalkulator:
             return "druga liczba to 0, nie dzieli się przez zero"
 
 try:
-    wpis1 = float(input("wpisz liczbę: "))
-    wpis2 = float(input("wpisz liczbę: "))
+    pierwszaWpisanaLiczba = float(input("wpisz liczbę: "))
+    drugaWpisanaLiczba = float(input("wpisz liczbę: "))
 
     testZera = Kalkulator(1, 0)
-    testUjemnej = Kalkulator(43, -2)
-    testUlamka = Kalkulator(0.5, 2)
-    testKombinacyjny = Kalkulator(-0.25, 49.12)
+    testKombinacyjny = Kalkulator(-0.25, 49)
 
-    kalkus = Kalkulator(wpis1, wpis2)
+    kalkus = Kalkulator(pierwszaWpisanaLiczba, drugaWpisanaLiczba)
 
     print(kalkus.dodawanie())
 
     assert testZera.dodawanie() == 1
-    assert testUjemnej.dodawanie() == 41
-    assert testUlamka.dodawanie() == 2.5
-    assert testKombinacyjny.dodawanie() == (49.12-0.25)
+    assert testKombinacyjny.dodawanie() == 48.75
 
     print(kalkus.odejmowanie())
 
     assert testZera.odejmowanie() == 1
-    assert testUjemnej.odejmowanie() == 45
-    assert testUlamka.odejmowanie() == -1.5
-    assert testKombinacyjny.odejmowanie() == 49.37
+    assert testKombinacyjny.odejmowanie() == 49.25
 
     print(kalkus.mnozenie())
 
     assert testZera.mnozenie() == 0
-    assert testUjemnej.mnozenie() == - 86
-    assert testUlamka.mnozenie() == 1
-    assert testKombinacyjny.mnozenie() == (49.12 * -0.25)
+    assert testKombinacyjny.mnozenie() == (49 * -0.25)
 
     print(kalkus.dzielenie())
 
     assert testZera.dzielenie() == "druga liczba to 0, nie dzieli się przez zero"
-    assert testUjemnej.dzielenie() == (43/-2)
-    assert testUlamka.dzielenie() == (0.5/2)
-    assert testKombinacyjny.dzielenie() == (-0.25/49.12)
+    assert testKombinacyjny.dzielenie() == (-0.25/49)
 
 except ValueError:
     print("nie wpisano liczb")
